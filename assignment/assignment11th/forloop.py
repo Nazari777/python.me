5aa2e6a9d7a1dd2b2bbe64965be1969b18a64551
nazari777 <shamir.nazari35@gmail.com>
2023-07-30 18:47:13 +0500

    assignment11th

--- assignment/assignment11th/forloop.py
+++ assignment/assignment11th/forloop.py
@@ -1,4 +1,6 @@
-k = 1 
-while k <= 454:
-    print(k)
-    k = k + 1
+numbers = [5, 2, 5, 2, 5]
+for x_count in numbers:
+    output = ''
+    for count in range(x_count):
+        output += 'x'
+    print(output)
